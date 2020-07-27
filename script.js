a0089b0f9a52397412a3fa1b8d4adef5597c8258
Abzed <ibraabzed@gmail.com>
2020-07-27 03:03:37 +0300

    add jQuery function for what we do toggle

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
-    $(".logoDez").click(function(){
-    alert("You ARe AY");
-    });
+    $(".change1").click(function () {
+        $(".change1").slideDown("1500").hide(1000);
+        $("#hide1").show("1500");
+      });
+      $("#hide1").click(function () {
+        $("#hide1").slideUp("1500").hide(1000);
+        $(".change1").slideDown("1500");
+      });
+
+      $(".change2").click(function () {
+        $(".change2").slideDown("1500").hide(1000);
+        $("#hide2").show("1500");
+      });
+      $("#hide2").click(function () {
+        $("#hide2").slideUp("1500").hide(1000);
+        $(".change2").slideDown("1500");
+      });
+
+      $(".change3").click(function () {
+        $(".change3").slideDown("1500").hide(1000);
+        $("#hide3").show("1500");
+      });
+      $("#hide3").click(function () {
+        $("#hide3").slideUp("1500").hide(1000);
+        $(".change3").slideDown("1500");
+      });
 });
